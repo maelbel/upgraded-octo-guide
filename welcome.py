6a053f464309e566039172a38db738b0b6cf0097
Belliard <mael.belliard@laposte.net>
2024-10-29 10:35:02 +0100

    Add demo python file

---
+++ welcome.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    saisie = input('Entrez oui pour valider : ').lower().strip()
+    if saisie == "oui":
+        print("Merci !")
+    else:
+        print("Je ne comprends pas !")
